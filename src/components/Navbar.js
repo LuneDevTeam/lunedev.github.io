a95e6657569e9e6cb31b7da910b558101205e3b1
RiegerJulio <riegerproducoes@gmail.com>
2021-12-21 18:41:11 -0300

    Navbar + NavbarCSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import stock from '../images/stockIMG.jpg';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
-class Navbar extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      click: false,
-      setClick: false,
-    }
-  }
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
 
-  render() {
+  const showSidebar = () => setSidebar(!sidebar);
+
     return (
-    <nav className="header">
-      <ul className="navbar">
-        <li><Link className="link-navbar" to="/">Home</Link></li>
-        <li><Link className="link-navbar" to="/about-us">Sobre Nós</Link></li>
-        <li><Link className="link-navbar" to="/projects">Projetos</Link></li>
-        <li><Link className="link-navbar" to="/contact">Contato</Link></li>
-      </ul>
-    </nav>
+    <div className="header">
+      <div className='navbar'>
+        <img src={stock} alt="lune-logo" className="logo-navbar"/>
+        <Link to='#' className="menu-bars">
+          <FaIcons.FaBars onClick={ showSidebar }/>
+        </Link>
+      </div>
+      <nav className={ sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          <li className="links-navbar"><Link className="nav-buttons" to="/">Home</Link></li>
+          <li className="links-navbar"><Link className="nav-buttons" to="/about-us">Sobre Nós</Link></li>
+          <li className="links-navbar"><Link className="nav-buttons" to="/projects">Projetos</Link></li>
+          <li className="links-navbar"><Link className="nav-buttons" to="/contact">Contato</Link></li>
+        </ul>
+      </nav>
+    </div>
     )
   }
-}
 
 export default Navbar;
