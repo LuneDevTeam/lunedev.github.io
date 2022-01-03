c8f207ea0fcdeb704ab56ef221bdb878e086be10
henriqueRubido <henriquerubido1@gmail.com>
2022-01-03 15:20:47 -0300

    soluciona o problema com as tags h1

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ export default class Title extends Component {
 	render() {
 		const { title } = this.props;
 		return (
-			<div className='title'>
-				<h1>{ title }</h1>
+			<div>
+				<h1 className='title'>{ title }</h1>
 			</div>
 		)
 	}
